7ffbb886a8159c6fd63784e73e3ae05dffcf3e62
Aaron Dargel <aaron@nervousyak.com>
2010-05-06 14:58:19 -0500

    Added solution_generator and specs.

---
+++ specs/solution_generator_specs.rb
@@ -0,0 +1,38 @@
+describe SolutionGenerator do
+  describe "#generate" do
+	context "when solution does not already exist" do
+	  before(:each) do
+	    @dir = double('Dir', :mkdir => true)
+	    @generator = SolutionGenerator.new(@dir);
+	  end
+	  it "creates a solution folder with the name of the solution." do
+	    @dir.should_receive(:mkdir).with("my_project")
+	    @generator.generate("my_project")
+	  end
+	  it "creates the src folder in the solution folder" do
+	    @dir.should_receive(:mkdir).with("my_project/src")
+		@generator.generate "my_project"
+	  end
+	  it "creates the lib folder in the solution folder" do
+	    @dir.should_receive(:mkdir).with("my_project/lib")
+		@generator.generate "my_project"
+	  end
+	  it "creates the doc folder in the solution folder" do
+	    @dir.should_receive(:mkdir).with("my_project/doc")
+		@generator.generate "my_project"
+	  end
+	  it "creates the test folder in the solution folder" do
+	    @dir.should_receive(:mkdir).with("my_project/test")
+		@generator.generate "my_project"
+	  end
+	  it "creates the build folder in the solution folder"  do
+	    @dir.should_receive(:mkdir).with("my_project/build")
+		@generator.generate "my_project"
+	  end
+	  it "creates the dist folder in the solution folder" do
+	    @dir.should_receive(:mkdir).with("my_project/dist")
+		@generator.generate "my_project"
+	  end
+	end
+  end
+end
